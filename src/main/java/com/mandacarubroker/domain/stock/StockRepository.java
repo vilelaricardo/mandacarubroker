21038d458ba37e356580691413fe81256d6461e6
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-02-07 10:07:26 -0300

    refactor: lint stock domain

--- src/main/java/com/mandacarubroker/domain/stock/StockRepository.java
+++ src/main/java/com/mandacarubroker/domain/stock/StockRepository.java
@@ -1,8 +1,8 @@
 package com.mandacarubroker.domain.stock;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface StockRepository  extends JpaRepository<Stock,String> {
+public interface StockRepository extends JpaRepository<Stock, String> {
 }
