513b027168e6bdefea4eba1c4197a3d7248b2af8
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-02-07 10:17:48 -0300

    refactor: rename main class using camel case

--- src/main/java/com/mandacarubroker/MandacaruBrokerApplication.java
+++ src/main/java/com/mandacarubroker/MandacaruBrokerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MandacarubrokerApplication {
+public class MandacaruBrokerApplication {
     public static void main(final String[] args) {
-        SpringApplication.run(MandacarubrokerApplication.class, args);
+        SpringApplication.run(MandacaruBrokerApplication.class, args);
     }
 }
