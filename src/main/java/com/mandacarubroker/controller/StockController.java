6e31e49ec9bc7be99a9cc3c532c6ec4dac066a24
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-02-07 10:07:56 -0300

    refactor: lint stock controller

--- src/main/java/com/mandacarubroker/controller/StockController.java
+++ src/main/java/com/mandacarubroker/controller/StockController.java
@@ -1,13 +1,20 @@
 package com.mandacarubroker.controller;
 
+import com.mandacarubroker.domain.stock.RequestStockDTO;
+import com.mandacarubroker.domain.stock.Stock;
+import com.mandacarubroker.service.StockService;
 
-import com.mandacarubroker.domain.stock.*;
-import com.mandacarubroker.service.*;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/stocks")
@@ -15,8 +22,8 @@ public class StockController {
 
     private final StockService stockService;
 
-    public StockController(StockService stockService) {
-        this.stockService = stockService;
+    public StockController(final StockService receivedStockService) {
+        this.stockService = receivedStockService;
     }
 
     @GetMapping
@@ -25,24 +32,23 @@ public class StockController {
     }
 
     @GetMapping("/{id}")
-    public Stock getStockById(@PathVariable String id) {
+    public Stock getStockById(@PathVariable final String id) {
         return stockService.getStockById(id).orElse(null);
     }
 
     @PostMapping
-    public ResponseEntity<Stock> createStock(@RequestBody RequestStockDTO data) {
+    public ResponseEntity<Stock> createStock(@RequestBody final RequestStockDTO data) {
         Stock createdStock = stockService.createStock(data);
         return ResponseEntity.ok(createdStock);
     }
 
     @PutMapping("/{id}")
-    public Stock updateStock(@PathVariable String id, @RequestBody Stock updatedStock) {
+    public Stock updateStock(@PathVariable final String id, @RequestBody final Stock updatedStock) {
         return stockService.updateStock(id, updatedStock).orElse(null);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStock(@PathVariable String id) {
+    public void deleteStock(@PathVariable final String id) {
         stockService.deleteStock(id);
     }
-
 }
