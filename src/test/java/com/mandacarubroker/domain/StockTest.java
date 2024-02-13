7fed056413781b690255d0e9f21698106052ba4d
peaga <pauloherbert@alu.ufc.br>
2024-02-12 21:30:13 -0300

    add:unit tests

---
+++ src/test/java/com/mandacarubroker/domain/StockTest.java
@@ -0,0 +1,48 @@
+package com.mandacarubroker.domain;
+
+import com.mandacarubroker.domain.stock.RequestStockDTO;
+import com.mandacarubroker.domain.stock.Stock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+class StockTest {
+    private Stock stock;
+
+    @BeforeEach
+    void setup(){
+        stock = new Stock("1","ABC1","Banco do Brasil Sa",50.0);
+    }
+
+    @Test
+    void constructorShouldReturnStockWhenStockDTOIsAParameter(){
+        Stock stockFromDTo = new Stock(new RequestStockDTO("ABC1","Banco do Brasil Sa",50.0));
+        assertNotNull(stockFromDTo);
+        assertEquals("ABC1", stockFromDTo.getSymbol());
+        assertEquals("Banco do Brasil Sa", stockFromDTo.getCompanyName());
+        assertEquals(50.0, stockFromDTo.getPrice());
+    }
+
+    @Test
+    void changePriceShouldReturnIncreasedPriceWhenIncreaseIsTrue(){
+        double expectedPrice = 60.0;
+        double actual = stock.changePrice(10.0,true);
+        assertEquals(expectedPrice,actual);
+    }
+
+    @Test
+    void changePriceShouldReturnDecreasedPriceWhenIncreaseIsFalse(){
+        double expectedPrice = 40.0;
+        double actual = stock.changePrice(10.0,false);
+        assertEquals(expectedPrice,actual);
+    }
+
+    @Test
+    void changePriceShouldReturnPriceWhenIncreaseIsFalseAndResultIsNegative(){
+        double expectedPrice = stock.getPrice();
+
+        double actual = stock.changePrice(60.0,false);
+        assertEquals(expectedPrice,actual);
+    }
+}
