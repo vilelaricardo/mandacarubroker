e20cd465249838f81017cbe07e4b4159cd3f381d
Maria Rita Moreira <irisccosta55@gmail.com>
2024-02-18 13:48:02 -0300

    refactor\correctedCodeStockServiceUpdateStock

--- src/main/java/com/mandacarubroker/service/StockService.java
+++ src/main/java/com/mandacarubroker/service/StockService.java
@@ -75,7 +75,6 @@ public class StockService {
                     stock.setSymbol(updatedStock.getSymbol());
                     stock.setCompanyName(updatedStock.getCompanyName());
                     stock.setPrice(updatedStock.getPrice());
-
                     return stockRepository.save(stock);
                 });
     }
