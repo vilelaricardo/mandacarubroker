a6ea1788592d23db6734b9b433c457dc8aa7b38f
Maria Rita Moreira <irisccosta55@gmail.com>
2024-02-18 13:07:18 -0300

    refactor\correctingStockController

---
+++ src/test/java/com/mandacarubroker/controller/StockControllerTest.java
@@ -0,0 +1,24 @@
+package com.mandacarubroker.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockControllerTest {
+
+    @Test
+    void getAllStocks() {
+    }
+
+    @Test
+    void getStockById() {
+    }
+
+    @Test
+    void createStock() {
+    }
+
+    @Test
+    void updateStock() {
+    }
+}
