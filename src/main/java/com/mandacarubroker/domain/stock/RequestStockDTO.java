604b6eb2df1cebb18fd0c6f2505f5372f16ae1ef
José Victor <victorlimaf55@gmail.com>
2024-02-18 22:19:35 -0300

    Update RequestStockDTO.java
    Adicionando validação de número positivo para o campo price.

--- src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
+++ src/main/java/com/mandacarubroker/domain/stock/RequestStockDTO.java
@@ -3,6 +3,7 @@ package com.mandacarubroker.domain.stock;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 public record RequestStockDTO(
         @Pattern(regexp = "[A-Za-z]{2}[0-9]{1}",
@@ -11,6 +12,7 @@ public record RequestStockDTO(
         @NotBlank(message = "Company name cannot be blank")
         String companyName,
         @NotNull(message = "Price cannot be null")
+        @Positive(message = "Price cannot be negative or zero")
         double price
 ) {
 }
