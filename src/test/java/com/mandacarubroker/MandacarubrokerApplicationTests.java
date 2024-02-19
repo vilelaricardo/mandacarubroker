e025aff6bed29a4c5ac44b47aba7d8b88b009cc4
José Victor <victorlimaf55@gmail.com>
2024-02-18 22:19:50 -0300

    Update MandacarubrokerApplicationTests.java

--- src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -1,13 +1,18 @@
 package com.mandacarubroker;
 
+import com.mandacarubroker.controller.StockController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MandacarubrokerApplicationTests {
-
+	@Autowired
+	private StockController controller;
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(controller);
 	}
 
 }
