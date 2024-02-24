4ca48bd417b041b3c414a2385fbebfea7f4cf050
peaga <pauloherbert@alu.ufc.br>
2024-02-24 19:12:51 -0300

    add: User domain and service layers

---
+++ src/main/java/com/mandacarubroker/service/UserService.java
@@ -0,0 +1,59 @@
+package com.mandacarubroker.service;
+
+import com.mandacarubroker.domain.user.RequestUserDTO;
+import com.mandacarubroker.domain.user.ResponseUserDTO;
+import com.mandacarubroker.domain.user.User;
+import com.mandacarubroker.domain.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+    private static final String NOT_FOUND_MSG = "User Not Found";
+    
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElseThrow(()->new EntityNotFoundException(NOT_FOUND_MSG));
+    }
+
+    public ResponseUserDTO createUser(RequestUserDTO data) {
+        validateRequestUserDTO(data);
+        User newUser = new User(data);
+        return new ResponseUserDTO(userRepository.save(newUser));
+    }
+
+    private void validateRequestUserDTO(RequestUserDTO data) {
+        //validation method
+    }
+
+    public ResponseUserDTO updateUser(String id, RequestUserDTO updatedUser) {
+        validateRequestUserDTO(updatedUser);
+        User updatedUserEntity = userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(updatedUser.username());
+                    user.setPassword(updatedUser.password());
+                    user.setEmail(updatedUser.email());
+                    user.setFirstName(updatedUser.firstName());
+                    user.setLastName(updatedUser.lastName());
+                    user.setBirthDate(updatedUser.birthDate());
+                    user.setBalance(updatedUser.balance());
+                     return userRepository.save(user);
+                }).orElseThrow(()->new EntityNotFoundException(NOT_FOUND_MSG));
+        return new ResponseUserDTO(updatedUserEntity);
+    }
+
+    public void deleteUser(String id) {
+        if (!userRepository.existsById(id)){
+            throw new EntityNotFoundException(NOT_FOUND_MSG);
+        }
+        userRepository.deleteById(id);
+    }
+}
--- src/main/resources/db/migration/V1__create-table-stock.sql
+++ src/main/resources/db/migration/V1__create-table-stock.sql
@@ -1,6 +1,16 @@
-CREATE TABLE stock(
+CREATE TABLE tb_stock(
                       id VARCHAR PRIMARY KEY,
                       symbol VARCHAR NOT NULL,
                       company_name VARCHAR NOT NULL,
                       price FLOAT NOT NULL
+);
+CREATE TABLE tb_user(
+                        id VARCHAR PRIMARY KEY,
+                        username VARCHAR NOT NULL UNIQUE ,
+                        password VARCHAR NOT NULL,
+                        email VARCHAR NOT NULL UNIQUE ,
+                        first_name VARCHAR NOT NULL,
+                        last_name VARCHAR NOT NULL,
+                        birth_date DATE NOT NULL,
+                        balance FLOAT NOT NULL
 );
