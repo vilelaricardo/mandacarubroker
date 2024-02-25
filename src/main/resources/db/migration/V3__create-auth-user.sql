33cd2bdb10678335fdac29cfd5911969755f2504
Ingrid Lima <ibezerradelima@gmail.com>
2024-02-24 21:02:39 -0300

    Migration to create admin auth user

---
+++ src/main/resources/db/migration/V3__create-auth-user.sql
@@ -0,0 +1,11 @@
+INSERT INTO auth_users (
+    id,
+    login,
+    password,
+    role
+) VALUES (
+  'b1f7aee6-e047-40f7-89a2-00f13cdff896',
+  'admin',
+  '$2a$10$CdtUErYZAUcACgieYZKds.GhZCp4411agJCMh1/KrmU1mAhJAAere',
+  'ADMIN'
+);
