8c7606a9fd37a60922e358b6fe017244e1d39f2a
Ingrid Lima <ibezerradelima@gmail.com>
2024-02-24 21:17:27 -0300

    Create AuthUserRepository

---
+++ src/main/java/com/mandacarubroker/repository/AuthUserRepository.java
@@ -0,0 +1,13 @@
+package com.mandacarubroker.repository;
+
+import com.mandacarubroker.domain.authuser.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The Auth User Repository interface.
+ **/
+@Repository
+public interface AuthUserRepository extends JpaRepository<AuthUser, String> {
+  AuthUser findByLogin(String login);
+}
