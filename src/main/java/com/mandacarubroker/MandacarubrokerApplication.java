4a6052d181245eb8d91dad8782bb34ca4ad5e261
Ingrid Lima <ibezerradelima@gmail.com>
2024-02-25 13:14:26 -0300

    :books: docs: Increase in swagger documentation
    Added descriptions.

--- src/main/java/com/mandacarubroker/MandacarubrokerApplication.java
+++ src/main/java/com/mandacarubroker/MandacarubrokerApplication.java
@@ -1,9 +1,16 @@
 package com.mandacarubroker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SuppressWarnings("checkstyle:MissingJavadocType")
+@OpenAPIDefinition(info = @Info(
+    title = "Mandacaru Broker API",
+    version = "1",
+    description = "API desenvolvido para simular o funcionamento de um Home Broker")
+)
 @SpringBootApplication
 public class MandacarubrokerApplication {
   public static void main(String[] args) {
--- src/main/java/com/mandacarubroker/controller/StockController.java
+++ src/main/java/com/mandacarubroker/controller/StockController.java
@@ -5,6 +5,10 @@ import com.mandacarubroker.domain.stock.RequestStockDataTransferObject;
 import com.mandacarubroker.domain.stock.Stock;
 import com.mandacarubroker.service.StockService;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,27 +32,47 @@ public class StockController {
     this.stockService = stockService;
   }
 
+  @Operation(summary = "Get all stock registers", method = "POST")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "success"),
+  })
   @GetMapping
   public List<Stock> getAllStocks() {
     return stockService.getAllStocks();
   }
 
+  @Operation(summary = "Get specific stock", method = "POST")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "success"),
+  })
   @GetMapping("/{id}")
   public Stock getStockById(@PathVariable String id) {
     return stockService.getStockById(id).orElse(null);
   }
 
+  @Operation(summary = "Register a stock", method = "POST")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "success"),
+  })
   @PostMapping
   public ResponseEntity<Stock> createStock(@RequestBody RequestStockDataTransferObject data) {
     Stock createdStock = stockService.createStock(data);
     return ResponseEntity.ok(createdStock);
   }
 
+  @Operation(summary = "Update a stock", method = "POST")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "success"),
+  })
   @PutMapping("/{id}")
   public Stock updateStock(@PathVariable String id, @RequestBody Stock updatedStock) {
     return stockService.updateStock(id, updatedStock).orElse(null);
   }
 
+  @Operation(summary = "Delete a stock", method = "POST")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "success"),
+  })
   @DeleteMapping("/{id}")
   public void deleteStock(@PathVariable String id) {
     stockService.deleteStock(id);
