cfe8ef4290ce79dd0961951b2fbae71a641b20de
abnercoolman <105078332+abnercoolman@users.noreply.github.com>
2024-03-04 11:10:46 -0300

    feat: CRUD User
    Criação da funcionalidade “User”:
    
    - Pasta domain: novo pacote user para conter as classes, User e UserRepository, relacionada aos usuários;
    - Pasta dto: novo RequestUserDTO para conter os DTOs de requisição e resposta para usuários;
    - Pasta service: novo serviço UserService;
    - Pasta controller: novo controlador UserController para gerenciar as rotas de usuários;
    - Pasta db.migration: adição da tabela “users”.

--- src/main/resources/db/migration/V1__create-table-stock.sql
+++ src/main/resources/db/migration/V1__create-table-stock.sql
@@ -1,6 +1,17 @@
-CREATE TABLE stock(
+CREATE TABLE stock (
     id VARCHAR PRIMARY KEY,
     symbol VARCHAR NOT NULL,
     company_name VARCHAR NOT NULL,
     price FLOAT NOT NULL
+);
+
+CREATE TABLE users (
+    id VARCHAR PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL DEFAULT 0.00
 );
